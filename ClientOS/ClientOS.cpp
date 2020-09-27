c628092953d16fab8ae6294c08e48382d67c18a1
Ilya Buzunov <ilia.buzunov@mail.ru>
2020-09-27 13:55:29 +0300

    sql и немного qt

--- ClientOS/ClientOS.cpp
+++ ClientOS/ClientOS.cpp
@@ -1,7 +1,21 @@
 #include "ClientOS.h"
+#include <QtGui>
+#include <QtWidgets>
 
 ClientOS::ClientOS(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+    this->setFixedSize(900,600);
+
+    fromDB();
+}
+
+void ClientOS::fromDB()
+{
+    ui.comboBoxUom->addItem("test");
+
+    QList<QString> dsad;
+    dsad << "ex1" << "ex2";
+    ui.comboBoxProduct->addItems(dsad);
 }
